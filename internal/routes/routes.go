a6285ea4252d3f35a5622537239a494532d5e725
AkiLetschne <Ferdinand.Porsche@Student.HTW-Berlin.de>
2024-12-03 23:38:54 +0100

    added auth requirement to "/api/export-json" ep (#14)

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/supotsu-no-ochaya/backend/internal/api"
 )
@@ -9,5 +10,5 @@ func RegisterAPIRoutes(e *core.ServeEvent, app core.App) {
 	apiGroup := e.Router.Group("/api")
 
 	apiGroup.GET("/test", api.TestHandler(app))
-	apiGroup.GET("/export-json", api.ExportJSONHandler(app))
+	apiGroup.GET("/export-json", api.ExportJSONHandler(app)).Bind(apis.RequireAuth())
 }
