e6fb49eb4cf03a2bc36e7b3a21e9ce6455ebf2ee
Robert <games.robert2@t-online.de>
2024-12-14 20:46:06 +0100

    migrations

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"log"
-
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 	"github.com/supotsu-no-ochaya/backend/internal/hooks"
 	"github.com/supotsu-no-ochaya/backend/internal/routes"
+	_ "github.com/supotsu-no-ochaya/backend/migrations"
+	"log"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{})
 
 	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
 		routes.RegisterAPIRoutes(e, app)
